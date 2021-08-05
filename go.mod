7cdadec3bcc7f95f4eeb2b4d061110fa763a1134
zhenhua32 <zhenhua32@gmail.com>
2021-08-05 12:38:43 +0800

    下载所有章节

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.7.1 // indirect
+	github.com/Rican7/retry v0.3.0
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.3.6 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
--- main.go
+++ main.go
@@ -2,13 +2,71 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"reflect"
+	"time"
 
+	"github.com/Rican7/retry"
+	"github.com/Rican7/retry/backoff"
+	"github.com/Rican7/retry/jitter"
+	"github.com/Rican7/retry/strategy"
 	_ "github.com/zhenhua32/xingkong/internal/bookimpl"
 	_ "github.com/zhenhua32/xingkong/internal/searchimpl"
 	"github.com/zhenhua32/xingkong/pkg/book"
 	"github.com/zhenhua32/xingkong/pkg/search"
 )
+
+func download(r search.SearchResult) {
+	b := book.GBM.NewBook(r)
+
+	file, _ := os.OpenFile(r.BookName+".txt", os.O_CREATE|os.O_WRONLY, 0644)
+
+	cList, e := b.GetChapterList()
+	if e != nil {
+		fmt.Println(e)
+	}
+
+	fmt.Println("总章节数:", len(cList))
+	fmt.Println("开始下载每一个章节")
+
+	for i, c := range cList {
+		fmt.Println(i, c.Name)
+		// 写入标题
+		file.WriteString(c.Name + "\n")
+
+		var content string
+
+		seed := time.Now().UnixNano()
+		random := rand.New(rand.NewSource(seed))
+		err := retry.Retry(
+			func(attempt uint) error {
+				content, e = c.GetContent()
+				return e
+			},
+			strategy.Limit(3),
+			strategy.BackoffWithJitter(
+				backoff.BinaryExponential(400*time.Millisecond),
+				jitter.Deviation(random, 0.5),
+			),
+		)
+		if err != nil {
+			fmt.Println("获取失败, 错误是", err)
+			continue
+		}
+
+		// 写入内容
+		file.WriteString(content + "\n\n")
+		time.Sleep(time.Second)
+	}
+}
+
+func printStruct(s interface{}) {
+	v := reflect.ValueOf(s)
+	for i := 0; i < v.NumField(); i++ {
+		fmt.Println(v.Type().Field(i).Name, v.Field(i).Interface())
+	}
+}
 
 func main() {
 	g := search.GSE
@@ -17,35 +75,10 @@ func main() {
 	resultList, _ := g.Search("大奉打更人", 10)
 
 	fmt.Println("结果数量", len(resultList))
-	// for _, r := range resultList {
-	// 	v := reflect.ValueOf(r)
-	// 	for i := 0; i < v.NumField(); i++ {
-	// 		fmt.Println(v.Type().Field(i).Name, v.Field(i).Interface())
-	// 	}
-	// }
 
 	result1 := resultList[0]
 	fmt.Println(result1)
+	printStruct(result1)
 
-	b := book.GBM.NewBook(result1)
-
-	fmt.Println(b)
-
-	clist, e := b.GetChapterList()
-	if e != nil {
-		fmt.Println(e)
-	}
-
-	fmt.Println(len(clist))
-
-	c := clist[0]
-	fmt.Println(c)
-
-	content, e := c.GetContent()
-	if e != nil {
-		fmt.Println(e)
-	}
-	fmt.Println(content)
-
-	os.WriteFile("a.txt", []byte(content), 0644)
+	download(result1)
 }
